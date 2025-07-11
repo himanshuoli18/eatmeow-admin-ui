6fa67a47c1b8c504648c6cc27ea2087d8896f0de
Himanshu Oli <129107651+himanshuoli18@users.noreply.github.com>
2025-07-11 17:04:48 +0530

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://eatmeow-api-production.up.railway.app/api/users';
+const BASE_URL = 'https://eatmeow-api.onrender.com/api/users';
 
 export const getAllUsers = async () => {
     const response = await axios.get(BASE_URL);
